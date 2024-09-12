9a45e7b4a3f413d819c3a1e24442aeaaaf82233f
Neeraj Fulpatiya <128641556+Neeraj-Fulpatiya@users.noreply.github.com>
2024-09-13 00:00:28 +0530

    Update vite.config.js
    updated the vite.config.js for the deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +21,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    rollupOptions: {
+      external: ['@uiw/react-md-editor']
+    }
+  }
+});
